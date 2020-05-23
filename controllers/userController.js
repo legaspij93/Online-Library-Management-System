51b09e57028a812cda9c84463ee9534b7cad0ecb
sean.yuhico <sean.yuhico@gmail.com>
2020-05-23 14:19:36 +0800

    initial validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,12 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 const bodyparser = require("body-parser")
+const session = require('express-session')
 const passport = require("passport")
 const crypto = require("crypto")
 const { ensureAdmin } = require("../helpers/auth")
+
+const { check , validationResult } = require('express-validator')
 
 const app = express()
 
@@ -22,20 +25,43 @@ router.get("/login", function(req,res){
     res.render("login.hbs")
 })
 
-router.post("/register", function(req, res){
-    var user = {
-        firstName : req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        ID: req.body.ID,
-        userType: 1
-    }
-    User.create(user).then((user)=>{
-        console.log(user)
-        res.redirect("/user/login") 
-    })
+router.post("/register", [
+    check('firstName').not().isEmpty().isAlpha().withMessage('First name is required'),
+    check('firstName').isAlpha().withMessage('Input should be characters only'),
+    check('lastName').isAlpha().withMessage('Last name is required'),
+    check('lastName').not().isEmpty().isAlpha().withMessage('Input should be characters only'),
+    check('username').not().isEmpty().withMessage('Username is required'),
+    check('password').not().isEmpty().withMessage('Password is required'),
+    check('password').isLength({min: 8}).withMessage('Password should be at least 8 characters'),
+    check('email').isEmail().withMessage('Email is required'),
+    check('ID').not().isEmpty().isInt().withMessage('ID is required')
+], function(req, res){
+        var errors = validationResult(req).array()
+        if(errors){
+            req.session.errors = errors
+            req.session.success = false
+            errors.forEach(element => {
+                console.log(element)
+            });
+
+            res.redirect('./register')
+        }else{
+            req.session.success = true
+            var user = {
+                firstName : req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username,
+                password: req.body.password,
+                email: req.body.email,
+                ID: req.body.ID,
+                userType: 1
+            }
+            User.create(user).then((user)=>{
+                console.log(user)
+                res.redirect("/user/login") 
+            })
+        }
+        
 })
 
 router.post("/registerManager", ensureAdmin, function(req,res){
